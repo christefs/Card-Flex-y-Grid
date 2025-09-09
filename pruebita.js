99313b1ff92b0a2f59eaa382649c7a59ef3ce314
Christian Figueroa <christian-f79@hotmail.com>
2025-09-08 22:02:34 -0500

    Añadidos bloques en Javascript que permiten retirar o colocar imagen o texto al pulsar los links Leer mas o Leer menos

---
+++ pruebita.js
@@ -0,0 +1,11 @@
+
+
+        
+
+    const boton = document.getElementById('miBoton');
+    const elemento = document.getElementById('miElemento');
+
+    boton.addEventListener('click', function() {
+      elemento.classList.toggle('oculto');
+    });
+    
